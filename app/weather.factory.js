e507237ae3cc9c30155056c8b1d66039e7cbf14a
tork6730 <matt.torkelson@gmail.com>
2016-10-12 12:08:26 -0700

    Update weather.factory.js

--- app/weather.factory.js
+++ app/weather.factory.js
@@ -40,40 +40,4 @@
 
 
 
-
-
-// (function() {
-//     'use strict';
-
-//     angular
-//         .module('app')
-//         .factory('WeatherFactory', WeatherFactory);
-
-//     WeatherFactory.$inject = ['$http'];
-
-//     /* @ngInject */
-//     function WeatherFactory($http) {
-//         var service = {
-//             getWeather: getWeather
-//         };
-
-//         return service;
-
-//         ////////////////
-
-//         function getWeather() {
-
-//             return $http({
-//                 method: 'GET',
-//                 url: 'http://api.openweathermap.org/data/2.5/forecast/city?'
-//                 params: {
-//                     APPID:'3d6ecfa602f9d5c940f2d642e12e6bc0',
-//                     q: 'moscow'
-//                 },
-//             }).then(function(response){
-
-//                 return response.data;
-//             });
-//         }
-//     }
-// })();
+// REVIEWED - MT JDCT HN
